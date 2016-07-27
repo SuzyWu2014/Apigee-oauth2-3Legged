b7d9c4c4e692c60aed2f915a20e740e38fae9875
shujinwu <shujinwu@oregonstate.edu>
2016-07-26 18:33:26 -0700

    add js to redirect to login

---
+++ oauth2/apiproxy/resources/jsc/redirect-to-login.js
@@ -0,0 +1,28 @@
+if (context.flow == "PROXY_REQ_FLOW") {
+    var responseType = context.getVariable("request.queryparam.response_type");
+    var clientId = context.getVariable("request.queryparam.client_id");
+    var redirectUrl = context.getVariable("request.queryparam.redirect_uri");
+    var scope = context.getVariable("request.queryparam.scope");
+
+    var callbackUrl = "https://osu-test.apigee.net/v1/oauth2/auth-code?response_type=" + responseType + "&scope=" + scope + "&client_id=" + clientId + "&redirect_url=" + redirectUrl;
+    var service = encodeURIComponent(callbackUrl)
+    var loginUrl = "https://login.oregonstate.edu/cas-dev/login?service=" + service;
+
+    context.setVariable("info.login_url", loginUrl);
+}
+
+if (context.flow == "TARGET_REQ_FLOW") {
+    var loginUrl = context.getVariable("info.login_url");
+
+    context.setVariable("target.url", loginUrl);
+    context.setVariable("target.copy.queryparams", true);
+    context.setVariable("target.copy.pathsuffix", true);
+}
+
+if (context.flow == "PROXY_RESP_FLOW") {
+    var loginUrl = context.getVariable("info.login_url");
+
+    context.setVariable("response.content", null);
+    context.setVariable("response.header.location", loginUrl);
+    context.setVariable("response.status.code", 302);
+}
